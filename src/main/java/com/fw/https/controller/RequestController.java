eb27d2d1f4658511beaeeed034c94814a1be9d6a
Pradip Mudi <pradipmudi24@gmail.com>
2021-02-12 01:01:06 +0530

    .

--- src/main/java/com/fw/https/controller/RequestController.java
+++ src/main/java/com/fw/https/controller/RequestController.java
@@ -2,28 +2,30 @@ package com.fw.https.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fw.https.model.ClientQuery;
 import com.fw.https.util.HttpUtil;
 
 @RestController
+@ControllerAdvice
 public class RequestController {
 	
 	@RequestMapping(
 		    value = "/response", 
 		    method = RequestMethod.GET)
-	@ResponseBody
-	public String getHttpsGetResponse(@RequestParam(value="q", required=false) String query, @RequestParam(value="client", required=false) String clientID ) throws IOException {
+	public String getHttpsGetResponse(@RequestParam(value="q", required=false) String query, @RequestParam(value="client", required=false) String clientID , HttpServletResponse response) throws IOException {
 		ClientQuery clientQuery = new ClientQuery();
 		clientQuery.setClientId(clientID);
 		clientQuery.setQuery(query);
-		
+		response.setStatus(HttpServletResponse.SC_ACCEPTED);
 		return HttpUtil.processRequest(clientQuery);
 	}
 	
@@ -32,9 +34,10 @@ public class RequestController {
 		    value = "/response", 
 		    method = RequestMethod.POST,
 		    consumes = "application/json")
-	@ResponseBody
-	public String getHttpsPostResponse(@RequestBody ClientQuery clientQuery) throws IOException {
+	public String getHttpsPostResponse(@RequestBody ClientQuery clientQuery,HttpServletResponse response) throws IOException {
+		response.setStatus(HttpServletResponse.SC_ACCEPTED);
 		return HttpUtil.processRequest(clientQuery);
 	}
+	
 
 }
